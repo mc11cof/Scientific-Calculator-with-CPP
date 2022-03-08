ac10ed75a95df21aa00ecab585ee1a7d9359fe57
mc11cof <marcalistaircof@gmail.com>
2022-03-08 18:48:10 -0500

    Pushing to github for the first time

---
+++ scientific.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Coffi All rights reserved.
+// Created by: Marc-Alistair Coffi
+// Date: March 2022
+/* 
+ This is a scientific calculator
+*/
+
+#include <iostream>
+#include <cmath>
+
+
+int main() {
+}
